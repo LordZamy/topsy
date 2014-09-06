baf0d381f98a30dd78044bdc12c19fbabd4c12d0
Samarth Wahal <samarthwahal@hotmail.com>
2014-09-06 12:49:52 -0500

    add tests

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var readline = require('readline');
+var Topsy = require('../lib/topsy');
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+var apiKey;
+
+console.log('WARNING: These tests consume your Topsy API credit.\n');
+
+rl.question('Please enter a Topsy API key.\n', function(answer) {
+	apiKey = answer;
+
+	var topsy = new Topsy(apiKey, 'json');
+
+	topsy.get('content/tweets', {q: 'obama', mintime: '1370217600', maxtime: '1370893329', include_metrics: '1'}, function(err, data) {
+		if(err) throw err;
+		console.log(data.results);
+	});
+});
